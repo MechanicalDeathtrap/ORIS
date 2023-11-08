2299fb95a20a30d9b63c090083a5da4275aab11f
Elizaveta Korikova <korikova-04@mail.ru>
2023-11-08 21:47:05 +0300

    Add files via upload

---
+++ Theme2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StripClub;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        Console.WriteLine(connectionString);
+
+        Console.Read();
+    }
+}
+
+
