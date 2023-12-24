afe0cda403724fbd0b775b519cc4d79b59362d37
MechanicalDeathtrap <korikova-04@mail.ru>
2023-12-24 14:46:11 +0300

    pushing hw

---
+++ Homework_3/MyHttpServer/MyHttpServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+HttpListener server = new HttpListener();
+
+server.Prefixes.Add("http://127.0.0.1:2323/");
+server.Start();
+Console.WriteLine("Серевер запущен");
+
+var context = await server.GetContextAsync();
+
+var response = context.Response;
+
+var filePath = Directory.GetCurrentDirectory() + @"\index.html";
+var responseData = File.ReadAllBytes(filePath);
+byte[] buffer = responseData;
+// получаем поток ответа и пишем в него ответ
+response.ContentLength64 = buffer.Length;
+using Stream output = response.OutputStream;
+// отправляем данные
+await output.WriteAsync(buffer);
+await output.FlushAsync();
+
+Console.WriteLine("Запрос обработан");
+
+server.Stop();
+Console.WriteLine("Сервер прекратил работу");
